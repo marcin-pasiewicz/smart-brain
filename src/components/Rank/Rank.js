68fa703f98a411a9aae2b312b2478da229167621
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2018-08-10 18:16:19 +0200

    add emoji rank based on lambda function

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
-      </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+class Rank extends PureComponent {
+    constructor(props){
+        super(props);
+        this.state = {
+            emoji: '',
+        }
+    }
+
+    generateEmoji = (entries) => {
+        fetch(`https://66x68ytow3.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+            .then(res => res.json())
+            .then(data => this.setState({emoji: data.input}))
+            .catch(err => console.log(err))
+    }
+
+    componentDidMount() {
+        const { entries } = this.props;
+
+        this.generateEmoji(entries)
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.entries !== this.props.entries) {
+            this.generateEmoji(this.props.entries)
+        }
+    }
+  render() {
+      const { name, entries } = this.props;
+      return (
+          <div>
+              <div className='white f3'>
+                  {`${name}, your current entry count is...`}
+              </div>
+              <div className='white f1'>
+                  {entries}
+              </div>
+              <div className='white f3'>
+                  {`Rank Badge ${this.state.emoji}`}
+              </div>
+          </div>
+      );
+  }
 }
 
 export default Rank;
